e6e35e651d22ea35aa980f6dd4d2ca68eebea69b
Kevin Isaac Montero Huerta <81937772+KIHM-02@users.noreply.github.com>
2022-02-27 21:37:56 -0600

    Agregacion de funciones y cambios en la estructura
    Se agregaron cambios a la estructura de la pagina, asi como esta version permite la busqueda de una determinada persona. 
    La insercion de personas sigue en proceso.

---
+++ Practica1.2/insertar2.php
@@ -0,0 +1,12 @@
+<?php 
+	if (isset($_POST['btnEnviar'])) 
+	{
+		$nombre  = $_POST['nombre'];
+		$carrera = $_POST['carrera'];
+		$dia  = $_POST['diaNacimiento'];
+		$mes  = $_POST['mesNacimiento'];
+		$anio = $_POST['anioNacimiento'];
+		
+	}
+
+?>
